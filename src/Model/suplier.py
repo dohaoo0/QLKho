c783406f66616adcdc07d438c96efb3ac7ee4ae0
dohaoo0 <dohao15102004@gmail.com>
2024-05-27 00:21:19 +0700

    #15 update suplier

--- src/Model/suplier.py
+++ src/Model/suplier.py
@@ -2,9 +2,12 @@ from src.Model.actor import Actor
 
 
 class Suplier(Actor):
+    def __init__(self, id=None, name=None, email=None, phone=None, address=None, contract=None):
+        super(Suplier, self).__init__(id, name, email, phone, address, contract)
+
     def insert_data(self):
         insert_query = f"\
-            INSERT INTO Suplier (IdSuplier, NameSuplier, Email, Phone, Address, ContractDate) \
+            INSERT INTO Suplier (IdSuplier, NameSuplier, Email, Phone, Address, Contract) \
             VALUES ('{self.id}', '{self.name}', '{self.email}', '{self.phone}', '{self.address}', '{self.contract}')"
         return insert_query
 
@@ -15,7 +18,7 @@ class Suplier(Actor):
     def update_data(self):
         update_query = f"UPDATE Suplier SET \
                         IdSuplier = '{self.id}', NameSuplier = '{self.name}', Email = '{self.email}',\
-                        Phone ='{self.phone}', Address = '{self.address}', ContractDate = '{self.contract}'"
+                        Phone ='{self.phone}', Address = '{self.address}', Contract = '{self.contract}'"
         return update_query
 
     def search_data(self, search_id=''):
