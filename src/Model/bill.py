4926d197cf98540bb2f0db75561db4a66ec30bc2
dohaoo0 <dohao15102004@gmail.com>
2024-05-27 00:18:42 +0700

    #7 update bill

--- src/Model/bill.py
+++ src/Model/bill.py
@@ -2,10 +2,11 @@ from src.Model.base import Base
 
 
 class Bill(Base):
-    def __init__(self, id, idactor, iduser, products, quantity, date):
+    def __init__(self, id=None, idactor=None, iduser=None, products=None, price=None, quantity=None, date=None):
         self.id = id
         self.idactor = idactor
         self.iduser = iduser
         self.products = products
+        self.price = price
         self.quantity = quantity
         self.date = date
